226b8510e88cde2cdd1faff03dcfc64b12541918
jay240322 <patel.jay.brij03@gmail.com>
2026-07-18 03:43:36 +0000

    feat: add pod details API endpoint

--- backend/app/kubernetes/pods.py
+++ backend/app/kubernetes/pods.py
@@ -37,3 +37,46 @@ def list_pods():
         })
 
     return pod_list
+
+def get_pod_details(namespace: str, pod_name: str):
+    api = get_k8s_client()
+
+    pod = api.read_namespaced_pod(
+        name=pod_name,
+        namespace=namespace
+    )
+
+    restart_count = 0
+
+    if pod.status.container_statuses:
+        restart_count = sum(
+            container.restart_count
+            for container in pod.status.container_statuses
+        )
+
+    created = pod.metadata.creation_timestamp
+
+    if created:
+        age = datetime.now(timezone.utc) - created
+        age = f"{age.days}d"
+
+    else:
+        age = "Unknown"
+
+    images = []
+
+    if pod.spec.containers:
+        images = [
+            container.image
+            for container in pod.spec.containers
+        ]
+
+    return {
+        "name": pod.metadata.name,
+        "namespace": pod.metadata.namespace,
+        "status": pod.status.phase,
+        "node": pod.spec.node_name,
+        "restarts": restart_count,
+        "age": age,
+        "images" : images,
+    } 
