919e1433b6e35694f400e7b712d06260a9366334
jay240322 <patel.jay.brij03@gmail.com>
2026-07-27 13:00:27 +0000

    feat(jwt): adding jwt for creat/login

--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -1,7 +1,13 @@
 from sqlalchemy.orm import Session
 
 from app.models.user import User
-from app.auth.security import hash_password
+
+from app.auth.security import (
+    hash_password,
+    verify_password,
+)
+
+from app.auth.jwt import create_access_token
 
 
 def register_user(
@@ -9,7 +15,7 @@ def register_user(
     username: str,
     email: str,
     password: str,
-) -> User:
+):
 
     existing_user = (
         db.query(User)
@@ -31,3 +37,34 @@ def register_user(
     db.refresh(user)
 
     return user
+
+
+def login_user(
+    db: Session,
+    email: str,
+    password: str,
+):
+
+    user = (
+        db.query(User)
+        .filter(User.email == email)
+        .first()
+    )
+
+    if not user:
+        raise ValueError("Invalid email or password")
+
+    if not verify_password(
+        password,
+        user.password_hash,
+    ):
+        raise ValueError("Invalid email or password")
+
+    token = create_access_token(
+        {
+            "sub": str(user.id),
+            "email": user.email,
+        }
+    )
+
+    return token
